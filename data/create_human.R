06c7493eba08ea4aeb21901517de4b2dc4096bf6
Leenapaivikki <Leenapaivikki@users.noreply.github.com>
2018-11-28 20:17:57 +0200

    Exercise5 update
    Updated files

--- data/create_human.R
+++ data/create_human.R
@@ -1,7 +1,8 @@
-# author: Leenapaivikki
-# date: 23112018  
+# author: Leena Huiku
+# date: 28112018  
+# original data source: http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt
 # This file contains the script of the Exercise 4 (wrangling 5) of IODS course.
-# The exercise in based on data from: United Nations Human Development Report 2015:
+# The exercise in based on data from: United Nations Human Development Report:
 # Human Development Index (HDI; http://hdr.undp.org/en/composite/HDI
 # Gender Inequality Index (GII; http://hdr.undp.org/en/composite/GII
 # Clear memory.
@@ -50,3 +51,28 @@ human <- inner_join(hd, gii, by = 'country')
 # Write the joined dataframe into a file.
 write.table(human, file = "human.csv", sep = "\t", col.names = TRUE)
 glimpse(human)
+
+
+#Exercise 5
+#Data wrangling part 
+
+# Transform the Gross National Income (GNI) variable to numeric
+human <- mutate(human, gni_cap = as.numeric(str_replace(human$gni_cap, pattern=",", replace ="")))
+# Exclude unneeded variables, columns to keep
+keep <- c('country','edu2_f_m','labo_f_m','edu_exp', 'life_exp','gni_cap','mat_mor','ado_birth','parli_f')
+# Select the 'keep' columns
+human <- select(human, one_of(keep))
+# Remove all rows with missing values
+human <- na.omit(human)
+# Remove all rows with missing values
+# Last 7 observations refer to larger areas than countries
+# Define the last indice we want to keep
+last <- nrow(human) - 7
+# Choose everything until the last 7 observations
+human <- human[1:last, ]
+#Define the row names of the data by the country names and remove the country name column from the data
+rownames(human) <- human$country
+human <- select(human, -country)
+# Save the human data in your data folder including the row names.
+# You can overwrite your old 'human' data
+write.table(human, file = "human.csv", sep = "\t", col.names = TRUE)
